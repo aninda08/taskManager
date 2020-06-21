6d506f3a85c11c15c484f88d89f8ee6bbd06872d
Aninda Chakraborty <aninda.death@gmail.com>
2020-06-21 12:24:56 +0530

    Adding Authentication to task and cascade delete all tasks for user delete

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -46,6 +47,12 @@ const userSchema = new mongoose.Schema({
     }]
 });
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+});
+
 userSchema.methods.generateAuthToken = async function() {
     const user = this;
     const token = jwt.sign({ _id: user._id.toString() }, 'Testing');
@@ -88,6 +95,14 @@ userSchema.pre('save', async function(next) {
 
     if(user.isModified('password'))
         user.password = await bcrypt.hash(user.password, 8);
+
+    next();
+});
+
+userSchema.pre('remove', async function(next) {
+    const user = this;
+
+    await Task.deleteMany({ owner: user._id});
 
     next();
 });
